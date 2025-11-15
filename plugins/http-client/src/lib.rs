28b0fd9e5a6cfd031bc31a88b6af384fdb34ea19
cc@ubuntu <thneonl@outlook.com>
2025-11-15 21:07:35 +0800

    protocol wit V0.0.1

--- plugins/http-client/src/lib.rs
+++ plugins/http-client/src/lib.rs
@@ -1,34 +1,36 @@
 use wit_bindgen::generate;
-
 generate!({
     world: "http-client",
     path: ["wit/world.wit",],
 });
-use crate::exports::component::http_client::protocol::Guest;
+use crate::exports::component::http_client::protocol::{Guest, GuestProtocolHttpClient};
+use component::http_client::config::Config;
+use component::http_client::flow::Res;
+
 use component::http_client::logging::{Level, log};
 struct Protocol;
+struct HttpClient;
+impl GuestProtocolHttpClient for HttpClient {
+    fn new(_name: String) -> Self {
+        HttpClient
+    }
+    fn register(&self, res: Res, config: Config) -> u32 {
+        log(
+            Level::Info,
+            &format!(
+                "Registering HTTP client with resource: {:?} and config: {:?}",
+                res, config
+            ),
+        );
+        42 // Placeholder handler ID
+    }
+    fn init(&self) {}
+    fn do_action(&self) {}
+    fn release(&self) {}
+    fn un_register(&self) {}
+}
 impl Guest for Protocol {
-    fn register() -> String {
-        log(Level::Info, "Registering Protocol");
-        "Protocol Registered".to_string()
-    }
-    fn init() -> String {
-        log(Level::Info, "Initializing Protocol");
-        "Protocol Initialized".to_string()
-    }
-    fn do_action() -> String {
-        log(Level::Info, "Doing Protocol Action");
-        "Protocol Action Done".to_string()
-    }
-    fn release() -> String {
-        log(Level::Info, "Releasing Protocol");
-        "Protocol Released".to_string()
-    }
-    fn un_register() -> String {
-        log(Level::Info, "Unregistering Protocol");
-        "Protocol Unregistered".to_string()
-    }
-
+    type ProtocolHttpClient = HttpClient;
 }
 
 export!(Protocol);
