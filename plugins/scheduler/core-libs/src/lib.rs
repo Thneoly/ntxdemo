ee6b5e521e5862a5cec87f3d9115dc6d9d420223
cc@ubuntu <thneonl@outlook.com>
2025-12-04 19:39:53 +0800

    s

--- plugins/scheduler/core-libs/src/lib.rs
+++ plugins/scheduler/core-libs/src/lib.rs
@@ -1,9 +1,7 @@
-pub mod error;
 pub mod ip;
 pub mod socket;
 
 pub mod component;
 
-pub use error::SchedulerError;
 pub use ip::{IpBinding, IpPool, IpPoolError, IpRange, PoolStats, ResourceType};
 pub use socket::{AddressFamily, Socket, SocketAddress, SocketError, SocketHandle, SocketProtocol};
